551fa01261fc9ca3f52c004261b64f4e28dc6530
Eric Irikannu <eeirikannu@gmail.com>
2020-06-06 00:02:12 +0000

    Formatted HTML pages and provided comments/documentation for loadHTML function.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,4 +1,13 @@
-function loadSelector(selector, filename) {
+/**
+ * Loads a selector from another HTML file so that it can be used.
+ *
+ * @param { string } selector - Selector refers to the HTML element.
+ * @param { string } filename - Filename of HTML file that is used to load the
+ *     element
+ *
+ * @example loadSelector("#social", "socials.html")
+ */
+function loadHTML(selector, filename) {
   $(document).ready(function() {
     $(selector).load(filename);
   });
